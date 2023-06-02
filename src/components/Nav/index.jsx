85ff6369f397ae04f6c74d655b3a50a5cc2ffeb0
Mathieu Dourlens <matdourlens95@gmail.com>
2023-06-02 19:11:42 +0200

    Ajout d'un reboot CSS et mise en page de Nav

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,16 +1,23 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
+import Logo from '../../assets/logo.png'
+import '../../styles/Nav.css'
 
 function Nav() {
+  const location = useLocation()
+
+  const activeLink = (path) => {
+    return !(location.pathname === path)
+  }
   return (
     <nav>
-      <img
-        src="../../assets/logo.png"
-        alt="Logo de Kasa"
-        className="nav-logo"
-      />
+      <img src={Logo} alt="Logo de Kasa" className="navLogo" />
       <div className="Navigation">
-        <Link to="/">Accueil</Link>
-        <Link to="/about">A propos</Link>
+        <Link to="/" className={activeLink('/') ? 'active' : 'home'}>
+          Accueil
+        </Link>
+        <Link to="/about" className={activeLink('/about') ? 'active' : 'about'}>
+          A propos
+        </Link>
       </div>
     </nav>
   )
--- src/index.jsx
+++ src/index.jsx
@@ -7,6 +7,7 @@ import About from './pages/About'
 import Product from './pages/Product'
 import Error from './pages/Error'
 import Footer from './components/Footer'
+import './styles/index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
