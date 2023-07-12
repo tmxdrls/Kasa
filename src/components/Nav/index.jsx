e6fe5cbe46b5f2adecfc7d974e19cc97858f3c9c
Mathieu Dourlens <matdourlens95@gmail.com>
2023-07-12 20:39:35 +0200

    Supression et useLocation qui ne servait plus grâce aux props

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,23 +1,14 @@
-import { Link, useLocation } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Logo from '../../assets/logo.png'
 import '../../styles/Nav.css'
 
 function Nav() {
-  const location = useLocation()
-
-  const activeLink = (path) => {
-    return !(location.pathname === path)
-  }
   return (
     <nav>
       <img src={Logo} alt="Logo de Kasa" className="navLogo" />
       <article className="Navigation">
-        <Link to="/" className={activeLink('/') ? 'active' : 'home'}>
-          Accueil
-        </Link>
-        <Link to="/about" className={activeLink('/about') ? 'active' : 'about'}>
-          A propos
-        </Link>
+        <Link to="/">Accueil</Link>
+        <Link to="/about">A propos</Link>
       </article>
     </nav>
   )
